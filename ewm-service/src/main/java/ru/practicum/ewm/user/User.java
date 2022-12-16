6d726ee14c5e8b07e69870c8988cea3d3cac385a
Roman_B <batrakov.r.v@yandex.ru>
2022-12-16 11:42:52 +0300

    creating users

---
+++ ewm-service/src/main/java/ru/practicum/ewm/user/User.java
@@ -0,0 +1,21 @@
+package ru.practicum.ewm.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email", unique = true)
+    private String email;
+}
---
+++ ewm-service/src/main/java/ru/practicum/ewm/user/service/UserService.java
@@ -0,0 +1,15 @@
+package ru.practicum.ewm.user.service;
+
+import java.util.List;
+
+public interface UserService {
+    UserDto getUserById(long id);
+
+    List<UserDto> getAllUsers();
+
+    UserDto createUser(UserDto userDto);
+
+    UserDto updateUser(long id, UserDto userDto);
+
+    void deleteUser(long id);
+}
